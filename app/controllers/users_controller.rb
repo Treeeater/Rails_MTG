4a888ce8d23debcfe7c2d82de6f878667196c7f6
Yuchen Zhou <yuchen@yuchen-VirtualBox.(none)>
2012-05-31 09:16:30 -0700

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  def new
+	@user = User.new
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      # Handle a successful save.
+	  sign_in @user
+	  flash[:success] = "Welcome to the MTG Draft!"
+	  redirect_to @user
+    else
+      render 'new'
+    end
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mtg::Application.config.secret_token = '894416d3630e6dd985d2b0a45312c52f99587c10ac88af5b9c3e23ff643eff724b32d5908cc8cea1d08ca8456bee3a7e940442a72f3ec5afa72782f8b0c187fd'
