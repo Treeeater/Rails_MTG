18b567f056a10ec8659b6fb4794b7eec8bc50336
Yuchen Zhou <yuchen@virginia.edu>
2012-06-06 01:07:46 -0700

    minor fix.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
-  $serverHostAddress = "http://localhost:3000/"
+  $hostServerAddress = "http://localhost:3000/"
 end
