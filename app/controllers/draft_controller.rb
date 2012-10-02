beed106326c3f3c02028e573110e4d789d990044
Yuchen Zhou <yz8ra@virginia.edu>
2012-10-02 00:27:38 -0400

    a bug found, but added to rdoc.

--- app/controllers/draft_controller.rb
+++ app/controllers/draft_controller.rb
@@ -7,8 +7,10 @@ class DraftController < ApplicationController
 	end
 
 	def new
-		arg = params.values.join(" ")
-		pid = Kernel.spawn("ruby ./draftServer/draftServer.rb " + current_user.id.to_s + " " + arg)
+		arg = params.values[0..-3].join(" ")
+		string = "ruby ./draftServer/draftServer.rb " + current_user.id.to_s + " " + arg
+		#File.open("log_draft_server.txt","w"){|f| f.write(string)}
+		pid = Kernel.spawn(string)
 		Process.detach(pid)
 	end
 	
