3f37ee1419b4b36fdf6d5e899ce004769826eb4c
Yuchen Zhou <yz8ra@virginia.edu>
2013-02-23 10:47:39 -0500

    fixed form validation for name, added no space and uniqueness.

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,10 +17,11 @@ class User < ActiveRecord::Base
   has_secure_password
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
-  validates :name,  presence: true, length: { maximum: 50 }
+  VALID_USERNAME_REGEX = /^\S*$/					#no space allowed.
+  validates :name,  presence: true, length: { maximum: 50 }, format: { with: VALID_USERNAME_REGEX, :message => ': no space allowed.' }, uniqueness: {case_sensitive: false}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-  
+
   private
 	def create_remember_token
 		self.remember_token = SecureRandom.urlsafe_base64
